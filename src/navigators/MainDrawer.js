89a762b4122e6b98a0efb81fc05c054b0009ac4d
Sapin Bajracharya <sbnull@gmail.com>
2018-01-06 13:16:48 +0545

    add navigation to settings

--- src/navigators/MainDrawer.js
+++ src/navigators/MainDrawer.js
@@ -1,9 +1,19 @@
 import { DrawerNavigator } from 'react-navigation'
 
 import HomeScreen from '../screens/HomeScreen'
+import OverviewScreen from '../screens/OverviewScreen'
+import SettingsScreen from '../screens/SettingsScreen'
 
 const MainDrawer = DrawerNavigator({
-  HomeScreen: { screen: HomeScreen }
+  HomeScreen: { screen: HomeScreen },
+  OverviewScreen: { screen: OverviewScreen },
+  SettingsScreen: { screen: SettingsScreen }
+}, {
+  initialRouteName: 'HomeScreen',
+  // https://github.com/react-navigation/react-navigation/issues/3148
+  drawerOpenRoute: 'DrawerOpen',
+  drawerCloseRoute: 'DrawerClose',
+  drawerToggleRoute: 'DrawerToggle'
 })
 
 export { MainDrawer }
